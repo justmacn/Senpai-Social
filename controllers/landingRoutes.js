3ab400ad3a7e27f5b586043382b399ee487beb5e
justmacn <172618812+justmacn@users.noreply.github.com>
2024-08-11 19:14:26 -0400

    fixed login container, added style to header/footer

--- controllers/landingRoutes.js
+++ controllers/landingRoutes.js
@@ -1,18 +1,17 @@
 const router = require("express").Router();
 
+// protects routes from unauthorized access
+const { withGuard } = require("../utils/authGuard");
+
 // add a get /login route here
 router.get("/", (req, res) => {
   try {
-    res.render("login");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// add a get /signup route here
-router.get("/signup", (req, res) => {
-  try {
-    res.render("signup");
+    // users still logged in will be redirected to home page instead of login page
+    if (withGuard === true) {
+      document.location.replace("/home")
+    } else {
+      res.render("login");
+    }
   } catch (err) {
     res.status(500).json(err);
   }
