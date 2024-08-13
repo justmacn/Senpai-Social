54b26b5b9ef0a99f164f74b5763d32699bf19893
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-08-13 13:21:01 -0400

    I made adjustments to the profile-routes page

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -1,40 +1,40 @@
 const router = require("express").Router();
 
 // import any models you plan to use for this page's routes here
-const { databyUser } = require("../models");
+const { User } = require("../models");
 
 // protects routes from unauthorized access
 const { withGuard } = require("../utils/authGuard");
 
 router.get("/", withGuard, async (req, res) => {
   try {
-    const data = await databyUser.findAll({
+    const data = await User.findAll({
       // Reminder- this is how you filter data by user_id
       where: {
-        user_id: req.session.user_id,
+        id: req.session.user_id,
       },
     });
-
-    const userExamples = data.map((example) =>
-      example.get({ plain: true })
-    );
+    
+    const user = data.get({ plain: true })
+    console.log("plain user", user)
+    
     // Reminder- We're passing the userExamples data to the page-one handlebars template here!
     // Reminder- We're also passing the loggedIn status to the page-one handlebars template here so that we can conditionally render items if the user is logged in or not.
-    res.render("page-one", {
-      userExamples,
+    res.render("profile", {
+      user,
       loggedIn: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
-});
+ });
 
 // route to edit user profile
 router.put("/edit", withGuard, async (req, res) => {
   try {
-    const { username, email } = req.body;
+    const { username, password, bio, profile_picture, favorite_anime } = req.body;
     const [updated] = await databyUser.update(
-      { username, email }, 
+      { username, password, bio, profile_picture, favorite_anime }, 
       {where: { user_id: req.session.user_id } }
     );
     if (updated) {
